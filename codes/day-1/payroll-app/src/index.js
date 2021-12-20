89a1880153baeb135db90ea2abd6055b71615206
siemens-c2c <joy_mon78@yahoo.co.in>
2021-12-20 17:49:26 +0530

    day-1 6th upload

--- codes/day-1/payroll-app/src/index.js
+++ codes/day-1/payroll-app/src/index.js
@@ -1,27 +1,56 @@
-function developer(name, id, basic, da, hra, incentive) {
+function employee(name, id, basic, da, hra) {
     this.name = name
     this.id = id
     this.basicPay = basic
     this.daPay = da
     this.hraPay = hra
-    this.incentivePay = incentive
     this.totalSalary = 0
+}
+employee.prototype.calculateSalary = function () {
+    this.totalSalary = this.basicPay + this.daPay + this.hraPay
+}
+
+function developer(name, id, basic, da, hra, incentive) {
+    employee.call(this, name, id, basic, da, hra)
+    this.incentivePay = incentive
 
     this.calculateSalary = function () {
-        this.totalSalary = this.basicPay + this.daPay + this.hraPay + this.incentivePay
+        employee.prototype.calculateSalary.call(this)
+        this.totalSalary += this.incentivePay
     }
 }
 
-const developers = [
+function hr(name, id, basic, da, hra, gratuity) {
+    employee.call(this, name, id, basic, da, hra)
+    this.gratuityPay = gratuity
+
+    this.calculateSalary = function () {
+        employee.prototype.calculateSalary.call(this)
+        this.totalSalary = this.totalSalary + this.gratuityPay
+    }
+}
+
+// console.log(Object.prototype)
+// console.log(developer.prototype)
+// console.log(hr.prototype)
+
+// const maheshObj = new developer('mahesh', 3, 1003, 2004, 3005, 4006)
+// maheshObj.calculateSalary()
+// console.log(maheshObj.totalSalary)
+
+
+const employees = [
     new developer('mahesh', 3, 1003, 2004, 3005, 4006),
     new developer('sunil', 2, 1001, 2002, 3003, 4004),
-    new developer('anil', 1, 1000, 2000, 3000, 4000)   
+    new developer('anil', 1, 1000, 2000, 3000, 4000),
+    new hr('banu',4,1050,2050,3050,4050),
+    new hr('ramnath',5,1150,2150,3150,4150)
 ]
 
 const salaryCallback = function (emp) {
     emp.calculateSalary()
 }
-developers.forEach(salaryCallback)
+employees.forEach(salaryCallback)
 
 const showCallback = function (emp) {
     console.log(`Name: ${emp.name}, Salary:${emp.totalSalary}`)
@@ -31,7 +60,7 @@ const showCallback = function (emp) {
 const salaryFilterCallback = function (emp) {
     return emp.totalSalary > 10010
 }
-const employeesFilteredBySalary = developers.filter(salaryFilterCallback)
+const employeesFilteredBySalary = employees.filter(salaryFilterCallback)
 //employeesFilteredBySalary.forEach(showCallback)
 
 const sortBySalaryCallback = function (emp1, emp2) {
@@ -43,5 +72,6 @@ const sortBySalaryCallback = function (emp1, emp2) {
 const sortByNameCallback = function (emp1, emp2) {
     return emp1.name.localeCompare(emp2.name)
 }
-developers.sort(sortByNameCallback)
-developers.forEach(showCallback)
+//employees.sort(sortByNameCallback)
+employees.forEach(showCallback)
+
