dc0853411b739e40561bf25a9b427b55d5b0cd80
siemens-c2c <joy_mon78@yahoo.co.in>
2021-12-21 18:28:12 +0530

    day-2 final upload

---
+++ codes/day-2/react-app-from-scratch/src/app/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+export function App() {
+    const message = 'welcome to react js'
+    return (
+        <div id='mainDiv'>
+            {message}
+        </div>
+    )
+}
